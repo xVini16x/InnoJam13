95c24e7b071d2b400994e6ba213bb8fcf1ec8892
Leon Arndt <leon.arndt@innogames.com>
2022-11-12 15:28:43 +0100

    add chicken animations

---
+++ Assets/Scripts/AllyView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AllyView : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private static readonly int Walk = Animator.StringToHash("Walk");
+
+    private void Start()
+    {
+        animator.SetBool(Walk, true);
+    }
+}
