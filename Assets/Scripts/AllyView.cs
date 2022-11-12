58b47911086647571861e38f5e8b8e7ba714a022
Vincenzo <vincenzo-a@hotmail.com>
2022-11-12 20:34:34 +0100

    feather vfx on collision

--- Assets/Scripts/AllyView.cs
+++ Assets/Scripts/AllyView.cs
@@ -1,10 +1,13 @@
+using System;
 using Events;
 using UniRx;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AllyView : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private NavMeshAgent navMesh;
 
     private static readonly int Walk = Animator.StringToHash("Walk");
 
@@ -21,5 +24,27 @@ public class AllyView : MonoBehaviour
             Position = transform.position,
             Type = ParticleType.FeatherExplosion
         });
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (navMesh.enabled || transform.parent!=null)
+        {
+            return;
+        }
+        if (collision.transform.TryGetComponent<BuildableArea>(out var area))
+        {
+            return;
+        }
+
+        if (collision.transform.CompareTag("Player"))
+        {
+            return;
+        }
+        MessageBroker.Default.Publish(new SpawnParticle
+                                      {
+                                          Position = transform.position,
+                                          Type = ParticleType.FeatherExplosion
+                                      });
     }
 }
