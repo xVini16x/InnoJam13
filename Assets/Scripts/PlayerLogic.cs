0cab93ab619e38eae8899be17f0e8cdaeefbc45f
Leon Arndt <leon.arndt@innogames.com>
2022-11-12 13:14:40 +0100

    players slowly dies when touching enemies

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 10f;
+
+    private float _health;
+
+    private void Start()
+    {
+        _health = maxHealth;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.transform.GetComponent<EnemyLogic>() != null)
+        {
+            // TODO: let enemy logic configure their attack strength
+            SetHealth(_health - 2f * Time.deltaTime);
+        }
+    }
+
+    private void SetHealth(float health)
+    {
+        _health = health;
+        if (_health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
