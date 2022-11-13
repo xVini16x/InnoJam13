53427a1aa45408fc768c71fb52b5a7731516d947
Leon Arndt <leon.arndt@innogames.com>
2022-11-13 17:17:51 +0100

    wolves destroy houses

--- Assets/Scripts/World/AllySpawnerLogic.cs
+++ Assets/Scripts/World/AllySpawnerLogic.cs
@@ -1,6 +1,7 @@
 using System;
 using UniRx;
 using UnityEngine;
+using UserInterface.View;
 using Random = UnityEngine.Random;
 
 namespace World
@@ -9,22 +10,28 @@ namespace World
     {
         [SerializeField] private GameObject[] allyPrefabs;
         [SerializeField] private float spawnIntervalSeconds;
-        public static  int maxAllyCount = 40;
+        public static int maxAllyCount = 40;
         public static int allyCount = 0;
         [SerializeField] private Transform spawnPosition;
         [SerializeField] private float Health = 10f;
 
         private float _health;
-        private void OnCollisionStay(Collision collisionInfo)
+
+        private void OnTriggerStay(Collider collisionInfo)
         {
-            if (collisionInfo.transform.TryGetComponent(out EnemyLogic enemyLogic))
+            if (collisionInfo.transform.TryGetComponent(out EnemyLogic _))
             {
                 SetHealth(_health - 2f * Time.deltaTime);
             }
         }
-        
+
         private void SetHealth(float health)
         {
+            if (health < _health)
+            {
+                transform.Shake();
+            }
+
             _health = health;
             if (_health <= 0f)
             {
@@ -36,6 +43,7 @@ namespace World
         {
             Destroy(gameObject, 2f);
         }
+
         private void Start()
         {
             _health = Health;
