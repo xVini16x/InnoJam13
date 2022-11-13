b3b47720f9337d4dccd96f762395017ae8b6b0f8
Leon Arndt <leon.arndt@innogames.com>
2022-11-13 15:12:44 +0100

    fmt

--- Assets/Scripts/UpgradeStation.cs
+++ Assets/Scripts/UpgradeStation.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Events;
 using UniRx;
 using UnityEngine;
@@ -10,7 +7,7 @@ using World;
 public class UpgradeStation : MonoBehaviour
 {
     [SerializeField] private UpgradeType _upgradeType;
-    [SerializeField] private ItemType  _costsItem;
+    [SerializeField] private ItemType _costsItem;
     [SerializeField] private int costAmount;
     [SerializeField] private InventorySystem _inventorySystem;
     [SerializeField] private GameObject blocker;
@@ -36,27 +33,28 @@ public class UpgradeStation : MonoBehaviour
             isValidTrigger = true;
         }
 
-        EnemyLogic enemyLogic=null;
+        EnemyLogic enemyLogic = null;
         if (_upgradeType != UpgradeType.UpgradeChicken)
         {
-            if (other.TryGetComponent<EnemyLogic>(out  enemyLogic))
+            if (other.TryGetComponent<EnemyLogic>(out enemyLogic))
             {
                 isValidTrigger = true;
             }
+
             if (other.TryGetComponent<NavMeshAgent>(out var agent))
             {
-                if (!agent.enabled && other.transform.parent!=null)
+                if (!agent.enabled && other.transform.parent != null)
                 {
                     isValidTrigger = false;
                 }
             }
-            
+
             if (other.CompareTag("Player"))
             {
                 isValidTrigger = true;
             }
         }
-        
+
         if (!isValidTrigger)
         {
             return;
@@ -65,13 +63,13 @@ public class UpgradeStation : MonoBehaviour
         if (!_inventorySystem.CouldUseItem(_costsItem, costAmount))
         {
             MessageBroker.Default.Publish(new SpawnParticle
-                                          {
-                                              Position = transform.position,
-                                              Type = ParticleType.UpgradeFailed
-                                          });
+            {
+                Position = transform.position,
+                Type = ParticleType.UpgradeFailed
+            });
             return;
         }
-        
+
         if (enemyLogic != null)
         {
             enemyLogic.Die();
@@ -81,12 +79,14 @@ public class UpgradeStation : MonoBehaviour
         {
             if (_upgradeType == UpgradeType.UpgradeChicken)
             {
-                rigidVelocity= allylogic.Rigid.velocity;
+                rigidVelocity = allylogic.Rigid.velocity;
                 position = allylogic.transform.position;
                 rotation = allylogic.transform.rotation;
             }
+
             allylogic.Die();
         }
+
         Upgrade();
     }
 
@@ -98,32 +98,34 @@ public class UpgradeStation : MonoBehaviour
     {
         switch (_upgradeType)
         {
-           case UpgradeType.ChickenCapacity:
-               AllySpawnerLogic.maxAllyCount += 2;
-               break;
-           case UpgradeType.UpgradeChicken:
-               var go=Instantiate(BiggerChicken, null);
-               AllySpawnerLogic.allyCount++;
-               if (go.TryGetComponent<Rigidbody>(out var rigidbody))
-               {
-                   rigidbody.velocity = rigidVelocity;
-               }
-               if (go.TryGetComponent<NavMeshAgent>(out var navMeshAgent))
-               {
-                   navMeshAgent.enabled = false;
-               }
-               go.transform.position = position;
-               go.transform.rotation = rotation;
-               break;
+            case UpgradeType.ChickenCapacity:
+                AllySpawnerLogic.maxAllyCount += 2;
+                break;
+            case UpgradeType.UpgradeChicken:
+                var go = Instantiate(BiggerChicken, null);
+                AllySpawnerLogic.allyCount++;
+                if (go.TryGetComponent<Rigidbody>(out var rigidbody))
+                {
+                    rigidbody.velocity = rigidVelocity;
+                }
+
+                if (go.TryGetComponent<NavMeshAgent>(out var navMeshAgent))
+                {
+                    navMeshAgent.enabled = false;
+                }
+
+                go.transform.position = position;
+                go.transform.rotation = rotation;
+                break;
         }
 
         _inventorySystem.TryUseItem(_costsItem, costAmount);
-        
+
         MessageBroker.Default.Publish(new SpawnParticle
-                                      {
-                                          Position = transform.position,
-                                          Type = ParticleType.Upgrade
-                                      });
+        {
+            Position = transform.position,
+            Type = ParticleType.Upgrade
+        });
     }
 }
 
